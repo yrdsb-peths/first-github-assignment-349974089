57bdefcfa4ff75236d608a699b325c10b3db65ec
349974089 <117937232+349974089@users.noreply.github.com>
2022-11-14 13:30:32 -0500

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -3,11 +3,6 @@ public class Rectangle
     private int width;
     private int height;
     
-    /**
-     * This is the constructor to create a Rectangle.
-     * To create a Rectangle we need to know its width
-     * and height.
-     */
     public Rectangle(int rectWidth, int rectHeight)
     {
         width = rectWidth;
